f57eebf79201e990768546b4e8d9c690f5d77e55
Marcelo <marcelo95326@icloud.com>
2022-04-15 00:14:20 -0400

    .gitIgnore
    created .gitignore file

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,36 @@
 const express = require("express");
+const { reset } = require("nodemon");
 const app = express();
 const { Sequelize } = require('sequelize');
 
-app.get('/', function (req, res) {
-    res.send('Hello 123')
+
+  const sequelize = new Sequelize('Running', 'root', 'imagine', {
+    host: 'localhost',
+    dialect: 'mysql' 
+  });
+  
+
+  app.get('/', function (req, res) {
+    try {
+      let dbName = sequelize.getDatabaseName();
+      console.log(dbName);
+      res.send(dbName);
+    } catch (error) {
+      console.log(error);
+    }
   })
 
 
+
+  // function  tryDB (){
+  //   try {
+  //     await sequelize.authenticate();
+  //     console.log('Connection has been established successfully.');
+  //   } catch (error) {
+  //     console.error('Unable to connect to the database:', error);
+  //   }
+
+  // }
+  
 
 app.listen(5000);
