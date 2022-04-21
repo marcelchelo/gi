675f7cafdd8f650ef488d3808a6d2d495ecb47a5
Marcelo <marcelo95326@icloud.com>
2022-04-21 16:00:31 -0400

    lets revert from this commit
    revert from this commit to 4fb4e13

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,24 +4,33 @@ import './css/About.css';
 
 function About () {
     return(
-    <div>
-        <div>
-            <h1> Marcelo Villalba</h1>
 
-        </div>
-        <div className="smBio" >
-            Aspiring to be better than yesterday
-        </div>
+
+        <div>
+        <p>About page </p>
+       </div>
+    )
+
+     
+    
+
+    // <div>
+    //     <div>
+    //         <h1> Marcelo Villalba</h1>
+
+    //     </div>
+    //     <div className="smBio" >
+    //         Aspiring to be better than yesterday
+    //     </div>
 
         
        
-        <div className='donotCenter'>
-            <p>This page is is my tying to figure out React. As i have never used it, i have decided to tinker with it. I hope it can grasp it asap  </p>
-        </div>
+    //     <div className='donotCenter'>
+    //         <p>This page is is my tying to figure out React. As i have never used it, i have decided to tinker with it. I hope it can grasp it asap  </p>
+    //     </div>
 
-    </div>
+    // </div>
        
-    )
       
 
 
