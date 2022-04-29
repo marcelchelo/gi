40e8f64700090042e39e225a370e2651d08c1247
Marcelo <marcelo95326@icloud.com>
2022-04-29 01:50:23 -0400

    ReactBootstrap
    using ReactBootstrap

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,9 +1,39 @@
 import React from 'react'; 
 import {NavLink} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import './Nav.css';
 
 function Nav() {
     return(
+
+        // <Navbar bg="light" expand="lg">
+        //         <Container>
+        //             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        //             <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        //             <Navbar.Collapse id="basic-navbar-nav">
+        //             <Nav className="me-auto">
+        //                 <Nav.Link href="#home">Home</Nav.Link>
+        //                 <Nav.Link href="#link">Link</Nav.Link>
+        //                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        //                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        //                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        //                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        //                 <NavDropdown.Divider />
+        //                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        //                 </NavDropdown>
+        //             </Nav>
+        //             </Navbar.Collapse>
+        //         </Container>
+        //     </Navbar>
+
+
+
+
+
+
         <nav>
             <h1>Marcelo-Villalba
             </h1>
@@ -26,6 +56,6 @@ function Nav() {
         </nav>
     );
 }
-Nav.propTypes = {};
+// Nav.propTypes = {};
 
 export default Nav;
